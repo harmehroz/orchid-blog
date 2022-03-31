ae0497029f459929fd780bd2be6994167e5c3c5f
harmehroz <harmehroz4091@gmail.com>
2022-03-31 21:14:35 +0530

    Final

---
+++ api/client/src/components/posts/Posts.jsx
@@ -0,0 +1,15 @@
+import Post from "../post/Post"
+import "./posts.css"
+
+export default function Posts({posts}) {
+  return (
+    <div className="posts">
+      {posts.map((p,key)=>(
+        <Post post={p} key={key} />
+      ))}
+      
+        
+</div>
+  );
+}
+
